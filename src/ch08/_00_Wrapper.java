80889b6fbb22b4b23f527205018e0ba1846ef032
bongbub <112855148+bongbub@users.noreply.github.com>
2025-06-09 10:49:45 +0900

    제발도래ㅏ

---
+++ src/ch08/_00_Wrapper.java
@@ -0,0 +1,20 @@
+package ch08;
+
+// 2025.6.9 (2교시)
+
+public class _00_Wrapper {
+	
+	/*
+	 기본형		Wrapper 클래스 (기본형의 첫글자가 대문자로 바뀜)
+	 boolean	Boolean
+	 byte		Byte
+	 char		Character(예외)
+	 short		Short
+	 int		Integer(예외)
+	 long		Long
+	 float		Float
+	 double		Double
+	 			String(기본형 없음. 원래 얘는 클래스임)
+	 */
+
+}
